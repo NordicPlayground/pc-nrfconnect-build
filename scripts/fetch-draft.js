75bf1e5196fca49bbe131d1203665d5f7920b89a
Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>
2019-06-12 16:10:23 +0200

    Use npx to locate node-pre-gyp

--- scripts/fetch-draft.js
+++ scripts/fetch-draft.js
@@ -55,9 +55,8 @@ if (!ghToken) {
 }
 
 const destDir = path.resolve(__dirname, '..', '..', '..', pkgJson.binary.module_path, '..');
-const nodePreGypPath = path.resolve('node_modules', '.bin', 'node-pre-gyp');
 
-const reveal = JSON.parse(cp.execSync(`${nodePreGypPath} reveal`));
+const reveal = JSON.parse(cp.execSync(`npx node-pre-gyp reveal`));
 
 const { name, package_name: packageName, module_name: moduleName, host } = reveal;
 const arr = host.split('/');
